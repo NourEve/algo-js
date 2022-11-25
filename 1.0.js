1abb8f7698f2c8292e319c6d0b92011cc35af24b
Nour Everaert <nour.everaert.prof@gmail.com>
2022-11-25 12:20:26 +0100

    exercice 3.0.

--- 1.0.js
+++
@@ -1,5 +0,0 @@
-console.log(10+5);
-console.log(10-5);
-console.log(10*5);
-console.log(10/5);
-console.log(11%5);
--- 1.2.js
+++
@@ -1,5 +0,0 @@
-let name = "Nour"
-let firstName = "Everaert"
-let city = "Nivelles"
-
-console.log("Your name is "+name+" "+firstName+" and you live in "+city+".")

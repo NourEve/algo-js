a0ab35634b1399ff4de1fb67c9b41ea3f04d3de7
Nour Everaert <nour.everaert.prof@gmail.com>
2022-11-26 16:00:15 +0100

    exercice 4.4.

--- Partie 4/4.4.js
+++ Partie 4/4.4.js
@@ -1,14 +1,11 @@
 let inputAr = ["Arno Volts","Aurélien Mariaule","Aurore Lemaire","Benjamin Porsont","Céline Verreydt","Corentin Miserque","Dominique Coppée","Edouard de Romrée de Vichenet","Hugo Goorickx","Jofrey Houyoux","Jonathan Manes","Jonathan bajoux","Laura Wilhelmi","Lysie Soyez","Marnie Benalia","Mathilde Cornelis","Milo Bonnet" ,"Nadim El Nakadi","Nathalie Vanden Abeele","Nathalie Goffette","Nour Everaert","Pierre Mauriello","Quentin Bource ","Virginie Dourson"];
 
-/*function pickLearner(array) {
-        for (let i=0; i < n; i++){
-            const map1 = array.map(Math.random()*array.length | 0);
-            return map1;
-        }
-}
-console.log(inputAr, 7);*/
-
-
+/**
+ * Create an array with "n" random elements of another array.
+ * @param {table} array - The base table.
+ * @param {number} n - The length of the new table.
+ * @return {table} - New array with "n" random elements.
+ */
 function pickLearner(array,n) {
     for (let i=0; i < n; i++){
     const random = [...array].sort(() => 0.5 - Math.random());
@@ -16,20 +13,3 @@ function pickLearner(array,n) {
     }
 }
 console.log(pickLearner(inputAr, 10));
-
-
-/*function pickLearner(array,n) {
-    for (let i=0; i < n; i++){
-        let random = array[Math.floor(Math.random() * array.lenght)];
-        let rValue = array [random];
-        return rValue;
-    }
-}
-let rValue = pickLearner(inputAr, 10);
-console.log(rValue);*/
-
-/*for (let elem of inputAr) {
-    console.log(elem);
-}*/
-
-
